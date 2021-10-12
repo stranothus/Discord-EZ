ceed61288b3ac9a3a32bdaae90a73145b373fcc5
stranothus <stranothus@icloud.com>
2021-10-12 12:49:08 -0500

    KA User Search

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
-const { Client, Intents } = require("discord.js");
-const dotenv = require("dotenv");
+import { Client, Intents, MessageEmbed } from "discord.js";
+import dotenv from "dotenv";
 import fetch from "node-fetch";
 
 dotenv.config();
@@ -8,7 +8,7 @@ const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAG
 
 const token = process.env.TOKEN;
 
-const api = "www.khanacademy.org/api/internal"
+const api = "https://www.khanacademy.org/api/internal"
 
 client.once("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -25,8 +25,17 @@ client.on("messageCreate", async msg => {
             case "kauser":
                 let endpoint = `${api}/user/profile?${/kaid/.test(args[0]) ? "kaid" : "username"}=${args[0]}&format=pretty`;
                 let data = await fetch(endpoint).then(response => response.json()).then(data => { return data; });
-                msg.channel.send(data);
+                let embed = new MessageEmbed()
+                    .setColor("#FAC000")
+                    .setTitle("**" + data.nickname + "**")
+                    .setURL("https://www.khanacademy.org/profile/" + data.kaid)
+                    .setDescription("**@" + data.username + "** - " + data.bio + "\n\n**Date Joined:** " + data.dateJoined + "\n**Energy Points:** " + data.points + "\n**Videos Completed:** " + data.countVideosCompleted + "\n**KAID:** " + data.kaid + "\n**Full data:** " + endpoint)
+                    .setThumbnail(data.avatarSrc);
+
+                msg.channel.send({ "embeds": [embed] });
             break;
+            default:
+                msg.channel.send("You can check my commands using =help");
         }
     }
 });
