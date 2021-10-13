0ab6c12a81725f94674cc98c6d79868319fd8482
stranothus <stranothus@icloud.com>
2021-10-13 10:44:48 -0500

    Linguistic Skillz implemented

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
-import { Client, Intents, MessageEmbed } from "discord.js";
+import { Client, Intents, MessageEmbed, MessageAttachment } from "discord.js";
 import imageToBase64 from "image-to-base64";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import ytsr from "ytsr";
+import translatte from "translatte";
 import dateToObj from "./dateToObj.js";
 import timeSince from "./timeSince.js";
 
@@ -11,7 +13,8 @@ const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAG
 
 const token = process.env.TOKEN;
 
-const api = "https://www.khanacademy.org/api/internal"
+const KAAPI = "https://www.khanacademy.org/api/internal"
+const DictAPI = "https://api.dictionaryapi.dev/api/v2/entries/en"
 
 client.once("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -20,13 +23,14 @@ client.once("ready", () => {
 client.on("messageCreate", async msg => {
     if(msg.content.startsWith("=")) {
         var command = msg.content.split(" ")[0].replace(/^=/, "");
-        var args = msg.content.split(" ").slice(1);
+        var args = msg.content.split(/("[^"]*")|\h+/).slice(1).filter(v => v);
+
         switch(command.toLowerCase()) {
             case "ping":
                 msg.channel.send("Pong!");
             break;
             case "kauser":
-                var endpoint = `${api}/user/profile?${/kaid/.test(args[0]) ? "kaid" : "username"}=${args[0]}&format=pretty`;
+                var endpoint = `${KAAPI}/user/profile?${/kaid/.test(args[0]) ? "kaid" : "username"}=${args[0]}&format=pretty`;
                 var data = await fetch(endpoint).then(response => response.json()).then(data => { return data; });
                 var joinedObj = dateToObj(data.dateJoined ? new Date(data.dateJoined) : new Date());
                 var sinceJoined = timeSince(joinedObj) || "Secret";
@@ -39,7 +43,7 @@ client.on("messageCreate", async msg => {
                 msg.channel.send({ "embeds": [embed] });
             break;
             case "kaprogram":
-                var endpoint = `${api}/scratchpads/${args[0]}?format=pretty`;
+                var endpoint = `${KAAPI}/scratchpads/${args[0]}?format=pretty`;
                 var data = await fetch(endpoint).then(response => response.json()).then(data => { return data; });
                 var authorEndpoint = `${api}/user/profile?kaid=${data.kaid}&format=pretty`;
                 var authorData = await fetch(authorEndpoint).then(response => response.json()).then(data => { return data; });
@@ -53,6 +57,52 @@ client.on("messageCreate", async msg => {
                     .setThumbnail(data.imageUrl)
 
                 msg.channel.send({ "embeds": [embed] });
+            break;
+            case "define":
+                var endpoint = `${DictAPI}/${args[0]}`;
+                var data = await fetch(endpoint).then(response => response.json()).then(data => { return data; });
+                var message = ``;
+                for(let i = 0; i < data[0].meanings.length; i++) {
+                    let index = data[0].meanings[i];
+
+                    message += `*${index.partOfSpeech}*:\n\n`;
+
+                    for(let e = 0; e < index.definitions.length; e++) {
+                        let endex = index.definitions[e];
+
+                        message += `${endex.definition}\n"${endex.example}"\n\n`;
+                    }
+                }
+
+                var embed = new MessageEmbed()
+                    .setColor("#00C0FA")
+                    .setTitle("**" + data[0].word.toUpperCase() + "**")
+                    .setDescription(message);
+
+                msg.channel.send({ embeds: [embed]});
+            break;
+            case "pronounce":
+                var results = await ytsr(`Pronounce ${args[0]}`, { limit:  1 });
+
+                msg.channel.send(results.items[0].url);
+            break;
+            case "translate":
+                var argsL = args.length,
+                    text,
+                    from;
+                if(argsL - 1) {
+                    from = args[0];
+                    text = args[1].substring(1, args[1].length - 1);
+                } else {
+                    from = "";
+                    text = args[0].substring(1, args[0].length - 1);
+                }
+
+                translatte(text, { to: 'en' }).then(res => {
+                    msg.channel.send(res.text + (from ? "" : ` (translated from ${res.from.language.iso})`));
+                }).catch(err => {
+                    console.error(err);
+                });
             break;
             default:
                 msg.channel.send("You can check my commands using =help");
