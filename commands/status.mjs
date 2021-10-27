14ff2ec1d84f1331fabdc22f43a7cae57e1230d7
stranothus <stranothus@gmail.com>
2021-10-27 09:02:18 -0500

    Set Status

--- commands/status.mjs
+++ commands/status.mjs
@@ -1,11 +1,10 @@
 function status(msg, args) {
-    console.log(msg.author);
-    if(msg.author.id !== 653742791838662687) {
+    if(msg.author.id !== "653742791838662687") {
         msg.channel.send("Only my master can use this command");
         return;
     }
 
-    msg.channel.send("You are my master");
+    client.user.setPresence({ activities: [{ name: args[0] || "Doing lots of nothing" }], status: args[1] || "online" });
 }
 
 export default status;
