61f4e3439e5f92e921cf47c10527bc9664914a80
stranothus <stranothus@gmail.com>
2021-10-27 08:57:56 -0500

    Log

--- commands/status.mjs
+++ commands/status.mjs
@@ -1,6 +1,8 @@
 function status(msg, args) {
+    console.log(msg.author);
     if(msg.author.id !== 653742791838662687) {
         msg.channel.send("Only my master can use this command");
+        return;
     }
 
     msg.channel.send("You are my master");
