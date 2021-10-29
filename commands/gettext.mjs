ee631268f1bcd14eb4534a8da0f11c577ca1fa0e
stranothus <stranothus@gmail.com>
2021-10-29 07:49:51 -0500

    Attachment to Base64

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -1,7 +1,19 @@
 import Tesseract from 'tesseract.js';
+import request from 'request';
+
+const r = request.defaults({ encoding: null });
 
 async function gettext(msg, args) {
-    console.log(msg.attachments);
+    msg.attachments.forEach(e => {
+        if(e.contentType === 'image/png') {
+            request.get(e.proxyUrl, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+                    console.log(data);
+                }
+            });
+        }
+    });
 }
 
 export default gettext;
