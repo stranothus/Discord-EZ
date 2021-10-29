0e93e47a0acbecf53d570d28d49505be5a94ef1b
stranothus <stranothus@gmail.com>
2021-10-29 08:37:49 -0500

    Async

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 async function gettext(msg, args) {
     console.log(msg.attachments);
-    msg.attachments.forEach(e => {
+    msg.attachments.forEach(async e => {
         if(e.contentType === 'image/png') {
             console.log("Converting...");
             console.log(await fetch(e.proxyUrl).then(r => r.buffer()).then(buf => `data:image/png;base64,` + buf.toString('base64')));
