ce12af828c66e2c687faea39046d226814f2f6b3
stranothus <stranothus@gmail.com>
2021-10-29 08:43:29 -0500

    Fixed Typo

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -5,8 +5,8 @@ async function gettext(msg, args) {
     console.log(msg.attachments);
     msg.attachments.forEach(async e => {
         if(e.contentType === 'image/png') {
-            console.log("Converting...");
-            console.log(await fetch(e.proxyUrl).then(r => r.buffer()).then(buf => `data:image/png;base64,` + buf.toString('base64')));
+            console.log("Converting " + e.proxyURL);
+            console.log(await fetch(e.proxyURL).then(r => r.buffer()).then(buf => `data:image/png;base64,` + buf.toString('base64')));
         } else {
             console.log(e.contentType + " is not supported");
         }
