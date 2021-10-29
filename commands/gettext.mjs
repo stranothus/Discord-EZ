6cc9f193430d206844350b1c71cc4f37b1536bf5
stranothus <stranothus@gmail.com>
2021-10-29 08:59:55 -0500

    Is B64 Valid

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -13,8 +13,9 @@ async function gettext(msg, args) {
     msg.attachments.forEach(async e => {
         if(e.contentType === 'image/png') {
             let base64 = await fetch(e.proxyURL).then(r => r.buffer()).then(buf => `data:image/png;base64,` + buf.toString('base64'));
-            let { data: { text } } = await worker.recognize(base64);
-            console.log(text);
+            console.log(base64);
+            let data = await worker.recognize(base64);
+            console.log(data);
         } else {
             console.log(e.contentType + " is not supported");
         }
