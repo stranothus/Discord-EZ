096dfd4b60e672d45f0729c15e456193550e7b55
stranothus <stranothus@gmail.com>
2021-10-29 07:54:27 -0500

    Log Updates

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -4,8 +4,10 @@ import request from 'request';
 const r = request.defaults({ encoding: null });
 
 async function gettext(msg, args) {
+    console.log(msg.attachments);
     msg.attachments.forEach(e => {
         if(e.contentType === 'image/png') {
+            console.log("Converting...");
             request.get(e.proxyUrl, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
