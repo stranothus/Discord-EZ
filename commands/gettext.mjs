a571aacb8bb732fda159d7a2379cb7665021fc50
stranothus <stranothus@gmail.com>
2021-10-29 08:06:58 -0500

    NodeFetch Instead

--- commands/gettext.mjs
+++ commands/gettext.mjs
@@ -1,21 +1,12 @@
 import Tesseract from 'tesseract.js';
-import http from 'http';
+import fetch from 'node-fetch';
 
 async function gettext(msg, args) {
     console.log(msg.attachments);
     msg.attachments.forEach(e => {
         if(e.contentType === 'image/png') {
             console.log("Converting...");
-            http.get(e.proxyUrl, (resp) => {
-                resp.setEncoding('base64');
-                body = "data:" + resp.headers["content-type"] + ";base64,";
-                resp.on('data', (data) => { body += data});
-                resp.on('end', () => {
-                    console.log(body);
-                });
-            }).on('error', (e) => {
-                console.log(`Got error: ${e.message}`);
-            });
+            await fetch(e.proxyUrl).then(r => r.buffer()).then(buf => `data:image/png;base64,` + buf.toString('base64'));
         } else {
             console.log(e.contentType);
         }
