10877cf848364585cb11d10d50bf31e7e20fedb2
stranothus <stranothus@gmail.com>
2021-11-10 07:58:26 -0600

    Censored Funny Texts

--- commands/funnytext/blarb.mjs
+++ commands/funnytext/blarb.mjs
@@ -1,11 +1,12 @@
 import asUser from "../../utils/asUser.mjs";
+import censor from "../../utils/censor.mjs";
 
 function blarb(msg, args) {
     let text = args.join(" ");
 
     text = text.split("").map(v => Math.round(Math.random()) ? v.toLowerCase() : v.toUpperCase()).join("");
 
-    asUser(msg.channel, msg.author, text);
+    asUser(msg.channel, msg.author, censor(text));
     
     msg.delete();
 }
---
+++ utils/censor.mjs
@@ -0,0 +1,8 @@
+function censor(text) {
+    let bannedwords = (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).bannedwords;
+    bannedwords.forEach(bannedword => text = text.replace(new RegExp("(" + bannedword + ")", "gi"), $1 => new Array($1.length + 1).join("\\*")));
+
+    return text;
+}
+
+export default censor;
