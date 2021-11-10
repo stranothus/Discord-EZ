0f5aecddfbe27753084ba572b9a5ac52b47cbd32
stranothus <stranothus@gmail.com>
2021-11-10 08:03:23 -0600

    Asynchronous

--- commands/funnytext/uwu.mjs
+++ commands/funnytext/uwu.mjs
@@ -1,12 +1,12 @@
 import asUser from "../../utils/asUser.mjs";
 import censor from "../../utils/censor.mjs";
 
-function uwu(msg, args) {
+async function uwu(msg, args) {
     let text = args.join(" ");
 
     text = text.replace(/(owo)|(ow)|(wo)|(o)/gi, "owo").replace(/(uwu)|(uw)|(wu)|(u)/gi, "uwu");
 
-    asUser(msg.channel, msg.author, censor(text));
+    asUser(msg.channel, msg.author, await censor(text));
     
     msg.delete();
 }
--- utils/censor.mjs
+++ utils/censor.mjs
@@ -1,4 +1,4 @@
-function censor(text) {
+async function censor(text) {
     let bannedwords = (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).bannedwords;
     bannedwords.forEach(bannedword => text = text.replace(new RegExp("(" + bannedword + ")", "gi"), $1 => new Array($1.length + 1).join("\\*")));
 
