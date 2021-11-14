5d8e65cf020217da06c973243543656b50ed1f62
stranothus <stranothus@gmail.com>
2021-11-14 09:27:29 -0600

    Commands aren't ghost pings Sometimes a command is deleted as part of the execution of the command. When this happens, it shouldn't be re-sent by the bot as a ghost ping if it pinged a user.

--- events/messageDelete.mjs
+++ events/messageDelete.mjs
@@ -1,9 +1,11 @@
 import asUser from "../utils/asUser.mjs";
 
 async function messageDelete(msg) {
-    let bannedwords = (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).bannedwords;
+    let guild = (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id }));
+    let bannedwords = guild.bannedwords;
+    let prefix = guild.prefix;
 
-    if(new Date().getTime() - msg.createdTimestamp < 100000 && (msg.mentions.everyone || msg.mentions.users.first() || msg.mentions.roles.first() || msg.type === "REPLY") && !new RegExp(bannedwords.join("|"), "i").test(msg.content)) {
+    if(new Date().getTime() - msg.createdTimestamp < 100000 && (msg.mentions.everyone || msg.mentions.users.first() || msg.mentions.roles.first() || msg.type === "REPLY") && !new RegExp(bannedwords.join("|"), "i").test(msg.content) && !msg.content.startsWith(prefix) && !msg.content.match(new RegExp("^<@!?" + client.user + ">\\s*"))) {
         asUser(msg.channel, msg.author, msg.type === "REPLY" ? `*[Replying to <@!${msg.author.id}>'s [Message](${hideLinkEmbed(`https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.reference.messageId}`)})]* - ${msg.content}` : msg.content);
     }
 }
