fb0ba759bd8ded2043ee313341eda50274228db0
stranothus <stranothus@gmail.com>
2021-11-15 14:53:41 -0600

    Remove logs

--- commands/reactrole/collect.mjs
+++ commands/reactrole/collect.mjs
@@ -8,7 +8,7 @@ function reactRole(msg, roles, reactions) {
             if(!user.bot) {
                 let member = await msg.guild.members.fetch(user.id);
                 let role = member.guild.roles.cache.find(role => role.name === roles[reactions.indexOf(reaction._emoji.name)]);
-
+                
                 member.roles.add(role);
             }
         });
@@ -17,7 +17,7 @@ function reactRole(msg, roles, reactions) {
             if(!user.bot) {
                 let member = await msg.guild.members.fetch(user.id);
                 let role = member.guild.roles.cache.find(role => role.name === roles[reactions.indexOf(reaction._emoji.name)]);
-
+                
                 member.roles.remove(role);
             }
         });
