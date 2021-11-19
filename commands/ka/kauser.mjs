5c40948aa3a650fdb40528b11293cbcc4889ca21
stranothus <stranothus@gmail.com>
2021-11-19 08:04:13 -0600

    Update kauser Khan Academy is changing their API. The old user endopoint no longer works so I had to update to the new one using a POST request.

--- commands/ka/kauser.mjs
+++ commands/ka/kauser.mjs
@@ -1,5 +1,5 @@
 import { MessageEmbed } from "discord.js";
-import getJSON from "./../../utils/getJSON.mjs";
+import fetch from "node-fetch";
 import dateToObj from "./../../utils/dateToObj.js";
 import timeSince from "./../../utils/timeSince.js";
 
@@ -11,8 +11,21 @@ async function kauser(msg, args) {
         return;
     }
 
-    let endpoint = `${KAAPI}/user/profile?${/kaid/.test(args[0]) ? "kaid" : "username"}=${args[0]}&format=pretty`;
-    let data = await getJSON(endpoint);
+    let data = (await fetch("https://www.khanacademy.org/api/internal/graphql/getFullUserProfile", {
+        "headers": {
+            "content-type": "application/json",
+            "cookie": "fkey=0",
+        },
+        "body": `{
+            \"operationName\":\"getFullUserProfile\",
+            \"variables\":{
+                \"username\":\"stranothus\"
+            },
+            \"query\":\"query getFullUserProfile($kaid: String, $username: String) {\\n  user(kaid: $kaid, username: $username) {\\n    id\\n    kaid\\n    key\\n    userId\\n    email\\n    username\\n    profileRoot\\n    gaUserId\\n    qualarooId\\n    isPhantom\\n    isDeveloper: hasPermission(name: \\\"can_do_what_only_admins_can_do\\\")\\n    isCurator: hasPermission(name: \\\"can_curate_tags\\\", scope: ANY_ON_CURRENT_LOCALE)\\n    isCreator: hasPermission(name: \\\"has_creator_role\\\", scope: ANY_ON_CURRENT_LOCALE)\\n    isPublisher: hasPermission(name: \\\"can_publish\\\", scope: ANY_ON_CURRENT_LOCALE)\\n    isModerator: hasPermission(name: \\\"can_moderate_users\\\", scope: GLOBAL)\\n    isParent\\n    isSatStudent\\n    isTeacher\\n    isDataCollectible\\n    isChild\\n    isOrphan\\n    isCoachingLoggedInUser\\n    canModifyCoaches\\n    nickname\\n    hideVisual\\n    joined\\n    points\\n    countVideosCompleted\\n    bio\\n    soundOn\\n    muteVideos\\n    showCaptions\\n    prefersReducedMotion\\n    noColorInVideos\\n    autocontinueOn\\n    newNotificationCount\\n    canHellban: hasPermission(name: \\\"can_ban_users\\\", scope: GLOBAL)\\n    canMessageUsers: hasPermission(name: \\\"can_send_moderator_messages\\\", scope: GLOBAL)\\n    isSelf: isActor\\n    hasStudents: hasCoachees\\n    hasClasses\\n    hasChildren\\n    hasCoach\\n    badgeCounts\\n    homepageUrl\\n    isMidsignupPhantom\\n    includesDistrictOwnedData\\n    preferredKaLocale {\\n      id\\n      kaLocale\\n      status\\n      __typename\\n    }\\n    underAgeGate {\\n      parentEmail\\n      daysUntilCutoff\\n      approvalGivenAt\\n      __typename\\n    }\\n    authEmails\\n    signupDataIfUnverified {\\n      email\\n      emailBounced\\n      __typename\\n    }\\n    pendingEmailVerifications {\\n      email\\n      unverifiedAuthEmailToken\\n      __typename\\n    }\\n    tosAccepted\\n    shouldShowAgeCheck\\n    __typename\\n  }\\n  actorIsImpersonatingUser\\n}\\n\"
+        }`,
+      "method": "POST"
+    })
+    .then(response => response.json())).data.user;
 
     if(!data) {
         msg.channel.send("The KA user could not be found. Please double check your query");
@@ -31,8 +44,7 @@ async function kauser(msg, args) {
             **Joined:** ${sinceJoined}
             **Energy Points:** ${Number(data.points).toLocaleString()}
             **Videos Completed:** ${Number(data.countVideosCompvared).toLocaleString()}
-            **KAID:** ${data.kaid}
-            **Full data:** ${endpoint}`);
+            **KAID:** ${data.kaid}`);
 
     msg.channel.send({ "embeds": [embed] });
 }
