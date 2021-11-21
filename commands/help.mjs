f98589fdaa38238c5b958867ff5034971b0ea5e7
stranothus <stranothus@gmail.com>
2021-11-21 17:07:07 -0600

    Send command help messages

--- commands/help.mjs
+++ commands/help.mjs
@@ -1,8 +1,7 @@
 import { MessageEmbed } from "discord.js";
 
 async function help(msg, args) {
-    let prefix = msg.guild ? (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).prefix : "{prefix}";
-    let message = "";
+    let prefix = msg.guild ? (await DB.Guilds.collection("In(fo").findOne({ "id": msg.guild.id })).prefix : "{prefix}";
     if(args.length) {
         switch(args[0]) {
             case "misc":
@@ -248,108 +247,108 @@ async function help(msg, args) {
             break;
             case "ping":
                 // tell how =ping works
-                message = `\`${prefix}ping\` makes me respond with \`Pong!\`.`;
+                msg.channel.send(`\`${prefix}ping\` makes me respond with \`Pong!\`.`);
             break;
             case "kauser":
                 // tell how =kauser works
-                message = `\`${prefix}kauser [username or kaid]\` makes me respond with the corresponding Khan Academy user information.`;
+                msg.channel.send(`\`${prefix}kauser [username or kaid]\` makes me respond with the corresponding Khan Academy user information.`);
             break;
             case "kaprogram":
                 // tell how =kaprogram works
-                message = `\`${prefix}kaprogram [program ID]\` makes me respond with the corresponding Khan Academy program information.`;
+                msg.channel.send(`\`${prefix}kaprogram [program ID]\` makes me respond with the corresponding Khan Academy program information.`);
             break;
             case "define":
                 // tell how =define works
-                message = `\`${prefix}define [word]\` makes me give a definition or set of definitions for a word.`;
+                msg.channel.send(`\`${prefix}define [word]\` makes me give a definition or set of definitions for a word.`);
             break;
             case "pronounce":
                 // tell how =pronounce works
-                message = `\`${prefix}pronounce [word]\` makes me link a YouTube video of how to pronounce your word.`;
+                msg.channel.send(`\`${prefix}pronounce [word]\` makes me link a YouTube video of how to pronounce your word.`);
             break;
             case "translate":
                 // tell how =translate works
-                message = `\`${prefix}translate [word(s)] [to]\* [from]\*\` makes me translate your word(s) into English. You can optionally specify a language. Make sure to wrap phrases in quotation marks or I'll only translate the first word.`
+                msg.channel.send(`\`${prefix}translate [word(s)] [to]\* [from]\*\` makes me translate your word(s) into English. You can optionally specify a language. Make sure to wrap phrases in quotation marks or I'll only translate the first word.`);
             break;
             case "reactrole":
                 // tell how =reactrole works
-                message = `\`${prefix}reactrole [role name] [emoji]...\` makes me make a reaction role. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`.
+                msg.channel.send(`\`${prefix}reactrole [role name] [emoji]...\` makes me make a reaction role. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`.
                 Don't worry if the role doesn't exist, I'll make it for you! Multiple reaction roles can be included in one message by repeating the format. Roles must be contained in quotation marks if spaces are present.
-                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {role} and {emoji} in the text to be replaced with each role and emoji.`
+                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {role} and {emoji} in the text to be replaced with each role and emoji.`);
             break;
             case "reactroleone":
                 // tell how =reactrole works
-                message = `\`${prefix}reactroleone [role name] [emoji]...\` makes me make a reaction role. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`.
+                msg.channel.send(`\`${prefix}reactroleone [role name] [emoji]...\` makes me make a reaction role. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`.
                 Don't worry if the role doesn't exist, I'll make it for you! Multiple reaction roles can be included in one message by repeating the format, but users can only react to have one of the roles. Roles must be contained in quotation marks if spaces are present.
-                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {role} and {emoji} in the text to be replaced with each role and emoji.`
+                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {role} and {emoji} in the text to be replaced with each role and emoji.`);
             break;
             case "poll":
                 // tell how =poll works
-                message = `\`${prefix}poll [option] [emoji]...\` makes me hold a poll. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`, but they can only react once, so voting is fair. Options must be contained in quotation marks if spaces are present.
-                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {option} and {emoji} in the text to be replaced with each option and emoji.`
+                msg.channel.send(`\`${prefix}poll [option] [emoji]...\` makes me hold a poll. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`, but they can only react once, so voting is fair. Options must be contained in quotation marks if spaces are present.
+                Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {option} and {emoji} in the text to be replaced with each option and emoji.`);
             break;
             case "diebot":
                 // tell how =diebot works
-                message = `\`${prefix}diebot\` makes me delete my messages`;
+                msg.channel.send(`\`${prefix}diebot\` makes me delete my messages`);
             break;
             case "clear":
                 // tell how =clear works
-                message = `\`${prefix}clear [number]\*\` makes me delete \`[number]\` messages. If number is not specified, I will delete 99 messages. Older messages might not be deleteable`;
+                msg.channel.send(`\`${prefix}clear [number]\*\` makes me delete \`[number]\` messages. If number is not specified, I will delete 99 messages. Older messages might not be deleteable`);
             break;
             case "clearall":
                 // tell how =clearall works
-                message = `\`${prefix}clearall\` makes me delete *every single message in the channel*. Use this command with caution!`;
+                msg.channel.send(`\`${prefix}clearall\` makes me delete *every single message in the channel*. Use this command with caution!`);
             break;
             case "mute":
                 // tell how =mute works
-                message = `\`${prefix}mute [user] [time]\* [reason]\*\` makes me mute a user for \`[time]\` of format \`[number][unit of time]\` or a default of one week`;
+                msg.channel.send(`\`${prefix}mute [user] [time]\* [reason]\*\` makes me mute a user for \`[time]\` of format \`[number][unit of time]\` or a default of one week`);
             break;
             case "unmute":
                 // tell how =unmute works
-                message = `\`${prefix}unmute [user]\` make me unmute a muted user early`;
+                msg.channel.send(`\`${prefix}unmute [user]\` make me unmute a muted user early`);
             break;
             case "addword":
                 // tell how =unmute works
-                message = `\`${prefix}addword [word]\` make me add a word to a list of censored words`;
+                msg.channel.send(`\`${prefix}addword [word]\` make me add a word to a list of censored words`);
             break;
             case "removeword":
                 // tell how =unmute works
-                message = `\`${prefix}removeword [word]\` make me remove a word from a list of censored words`;
+                msg.channel.send(`\`${prefix}removeword [word]\` make me remove a word from a list of censored words`);
             break;
             case "checkwords":
                 // tell how =unmute works
-                message = `\`${prefix}checkwords\` I'll send a list of censored words. Make sure to use this in a staff only channel!`;
+                msg.channel.send(`\`${prefix}checkwords\` I'll send a list of censored words. Make sure to use this in a staff only channel!`);
             break;
             case "prefix":
                 // tell how =prefix works
-                message = `\`${prefix}prefix [new prefix]\*\` I'll state my prefix or set it to a new one!`;
+                msg.channel.send(`\`${prefix}prefix [new prefix]\*\` I'll state my prefix or set it to a new one!`);
             break;
             case "warn":
                 // tell how =warn works
-                message = `\`${prefix}warn [user] [reason]\` makes me store a warning for [user] about [reason] to review for various purposes`;
+                msg.channel.send(`\`${prefix}warn [user] [reason]\` makes me store a warning for [user] about [reason] to review for various purposes`);
             break;
             case "infractions":
                 // tell how =infractions works
-                message = `\`${prefix}infractions [user]\` makes me list stored warnings/infractions of [user]`;
+                msg.channel.send(`\`${prefix}infractions [user]\` makes me list stored warnings/infractions of [user]`);
             break;
             case "gettext":
                 // tell how =gettext works
-                message = `\`${prefix}gettext\` makes me list the text from any PNG or JPEG files attached`;
+                msg.channel.send(`\`${prefix}gettext\` makes me list the text from any PNG or JPEG files attached`);
             break;
             case "funnytext":
                 // tell how =gettext works
-                message = `\`${prefix}funnytext [filters]...\* [message]\` makes me post a message from you made a little more fun. Use \`uwu\`, \`owo\`, \`blarb\`, \`piglatin\`, \`shakespeare\`, \`lego\`, or \`reverse\` or you can use multiple! Find out what each one does and use several for your own combination`;
+                msg.channel.send(`\`${prefix}funnytext [filters]...\* [message]\` makes me post a message from you made a little more fun. Use \`uwu\`, \`owo\`, \`blarb\`, \`piglatin\`, \`shakespeare\`, \`lego\`, or \`reverse\` or you can use multiple! Find out what each one does and use several for your own combination`);
             break;
             case "invite":
-                message = `\`${prefix}invite\` makes me give you my invite so you can add me to other servers`
+                msg.channel.send(`\`${prefix}invite\` makes me give you my invite so you can add me to other servers`);
             break;
             case "github":
-                message = `\`${prefix}invite\` makes me give you my GitHub repo link`
+                msg.channel.send(`\`${prefix}invite\` makes me give you my GitHub repo link`);
             break;
             case "avatar":
-                message = `\`${prefix}avatar [user]\*\` I'll send a larger picture of your avatar or the user mentioned in the command`
+                msg.channel.send(`\`${prefix}avatar [user]\*\` I'll send a larger picture of your avatar or the user mentioned in the command`);
             break;
             case "binary":
-                message = `\`${prefix}binary [text]\*\` I'll convert to or from binary`
+                msg.channel.send(`\`${prefix}binary [text]\*\` I'll convert to or from binary`);
             break;
         }
     } else {
