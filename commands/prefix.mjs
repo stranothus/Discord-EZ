829170ddf5aa97732a16b3f66f78db9229afaf4d
stranothus <stranothus@gmail.com>
2021-11-24 23:00:40 -0600

    Converted to proper export

--- commands/prefix.mjs
+++ commands/prefix.mjs
@@ -1,5 +1,6 @@
 import { Permissions } from "discord.js";
 import isAdmin from "../utils/isAdmin.mjs";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 async function prefix(msg, args) {
     if(!args[0]) {
@@ -13,4 +14,40 @@ async function prefix(msg, args) {
     msg.channel.send("My prefix is now `" + args[0] + "`");
 }
 
-export default prefix;
+export default {
+    data: new SlashCommandBuilder()
+        .setName("prefix")
+        .setDescription("Set and view the prefix of the server")
+        .addStringOption(option => option
+            .setName("prefix")
+            .setDescription("the new prefix to set")
+            .setRequired(false)
+        ),
+    description: `prefix [new prefix]\*\` I'll state my prefix or set it to a new one!`,
+    category: "settings",
+    DMs: false,
+    execute: async function(interaction) {
+        let newPrefix = interaction.options.getString("prefix");
+
+        if(!newPrefix) {
+            interaction.reply("My prefix is `" + (await DB.Guilds.collection("Info").findOne({ "id": interaction.guild.id })).prefix + "`");
+            return;
+        }
+    
+        if(!isAdmin(interaction)) return;
+    
+        DB.Guilds.collection("Info").updateOne({ "id": interaction.guild.id }, { "$set": { "prefix": newPrefix }}, () => {});
+        interaction.reply("My prefix is now `" + newPrefix + "`");
+    },
+    executeText: async function(msg, args) {
+        if(!args[0]) {
+            msg.channel.send("My prefix is `" + (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).prefix + "`");
+            return;
+        }
+    
+        if(!isAdmin(msg)) return;
+    
+        DB.Guilds.collection("Info").updateOne({ "id": msg.guild.id }, { "$set": { "prefix": args[0] }}, () => {});
+        msg.channel.send("My prefix is now `" + args[0] + "`");
+    }
+};
