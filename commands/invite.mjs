24ad8e804d045c248641d712ddb8fe0aea5fac71
stranothus <stranothus@gmail.com>
2021-11-25 19:41:32 -0600

    Converted to proper export

--- commands/invite.mjs
+++ commands/invite.mjs
@@ -1,5 +1,20 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
 function invite(msg, args) {
     msg.channel.send("https://discord.com/api/oauth2/authorize?client_id=886933964537880617&permissions=8&scope=bot");
 }
 
-export default invite;
+export default {
+    data: new SlashCommandBuilder()
+        .setName("invite")
+        .setDescription("Get my invite to add me to other servers"),
+    description: `invite\` makes me give you my invite so you can add me to other servers`,
+    category: "about",
+    DMs: true,
+    execute: function(interaction) {
+        interaction.reply("https://discord.com/api/oauth2/authorize?client_id=886933964537880617&permissions=8&scope=bot");
+    },
+    executeText: function(msg, args) {
+        msg.channel.send("https://discord.com/api/oauth2/authorize?client_id=886933964537880617&permissions=8&scope=bot");
+    }
+};
