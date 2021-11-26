61c85fb7b8f2718da0d50cc1044e510497c69797
stranothus <stranothus@gmail.com>
2021-11-25 19:40:48 -0600

    Converted to proper export

--- commands/github.mjs
+++ commands/github.mjs
@@ -1,5 +1,16 @@
-function github(msg, args) {
-    msg.channel.send("https://github.com/stranothus/Discord-EZ");
-}
+import { SlashCommandBuilder } from "@discordjs/builders";
 
-export default github;
+export default {
+    data: new SlashCommandBuilder()
+        .setName("github")
+        .setDescription("Get the link to my GitHub repoisitory"),
+    description: ``,
+    category: "about",
+    DMs: true,
+    execute: function(interaction) {
+        interaction.reply("https://github.com/stranothus/Discord-EZ");
+    },
+    executeText: function(msg, args) {
+        msg.channel.send("https://github.com/stranothus/Discord-EZ");
+    }
+};
