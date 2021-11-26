6f6df5cf0cd5d4e1ce64de67257e2331363d6b07
stranothus <stranothus@gmail.com>
2021-11-25 19:41:49 -0600

    Converted to proper export

--- commands/ping.mjs
+++ commands/ping.mjs
@@ -1,5 +1,16 @@
-function ping(msg, args) {
-    msg.channel.send("Pong!");
-}
+import { SlashCommandBuilder } from "@discordjs/builders";
 
-export default ping;
+export default {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with pong"),
+    description: `ping\` makes me respond with \`Pong!\`.`,
+    category: "misc",
+    DMs: true,
+    execute: function(interaction) {
+        interaction.reply("Pong!");
+    },
+    executeText: function(msg, args) {
+        msg.channel.send("Pong!");
+    }
+};
