6dfc6bc489aee72e7daeeda6d8f06ef891ff8cbc
stranothus <stranothus@gmail.com>
2021-11-27 11:22:24 -0600

    Forgot to delete the previous function

--- commands/prefix.mjs
+++ commands/prefix.mjs
@@ -2,18 +2,6 @@ import { Permissions } from "discord.js";
 import isAdmin from "../utils/isAdmin.mjs";
 import { SlashCommandBuilder } from "@discordjs/builders";
 
-async function prefix(msg, args) {
-    if(!args[0]) {
-        msg.channel.send("My prefix is `" + (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).prefix + "`");
-        return;
-    }
-
-    if(!isAdmin(msg)) return;
-
-    DB.Guilds.collection("Info").updateOne({ "id": msg.guild.id }, { "$set": { "prefix": args[0] }}, () => {});
-    msg.channel.send("My prefix is now `" + args[0] + "`");
-}
-
 export default {
     data: new SlashCommandBuilder()
         .setName("prefix")
