5bc7e25c198adba123bfcc33635e1583cdd31169
stranothus <stranothus@gmail.com>
2021-11-27 11:22:03 -0600

    Remove from slash commands DiscordJS does not support variadic arguments in slash commands, so it does not make sense to try to include commands which need variadics. A message suggesting to use text commands instead is sent.

--- commands/poll.mjs
+++ commands/poll.mjs
@@ -1,62 +1,17 @@
-import pollCollect from "./collect.mjs";
+import pollCollect from "../utils/poll.mjs";
 import { SlashCommandBuilder } from "@discordjs/builders";
-import deQuote from "../../utils/deQuote.mjs";
+import deQuote from "../utils/deQuote.mjs";
 
 export default {
     data: new SlashCommandBuilder()
         .setName("poll")
-        .setDescription("create a poll for users to vote in")
-        .addStringOption(option => option
-            .setName("content")
-            .setDescription("option emoji option emoji etc")
-        ),
+        .setDescription("create a poll for users to vote in"),
     description: `poll [option] [emoji]...\` makes me hold a poll. Users can react with \`[emoji]\` on the message to make me give them \`[role name]\`, but they can only react once, so voting is fair. Options must be contained in quotation marks if spaces are present.
     Optionally, add a final argument at the end. This should be a string of text to format each option off of. Use {option} and {emoji} in the text to be replaced with each option and emoji.`,
     category: "misc",
     DMs: false,
     execute: async function(interaction) {
-        let content = interaction.options.getString("content").split(/("[^"]*")|\s+/).filter(v => v).map(v => deQuote(v));
-        let format = content.length % 2 ? content[content.length - 1] : false;
-    
-        if(content.length < 2) {
-            interaction.reply({ content: "Malformed command", ephemeral: true });
-            return;
-        }
-        
-        if(format) {
-            content.pop();
-        }
-    
-        let msgs = content
-            .map((v, i, a) => ((i % 2) ? undefined : {
-                content: format ? format.replace(/{option}/g, v).replace(/{emoji}/g, a[i + 1]) : `${v} ${a[i + 1]}`,
-                emoji: a[i + 1],
-                option: v
-            }))
-            .filter(v => v);
-    
-        let contents = msgs.map(v => v.content);
-        let emojis = msgs.map(v => v.emoji);
-        let options = msgs.map(v => v.role);
-    
-        let msg = await interaction.reply(content.join("\n"));
-    
-        DB.Guilds.collection("Info").updateOne({ "id": msg.guild.id }, { "$push": { "polls": {
-            messageID: msg.id,
-            channelID: msg.channel.id,
-            reactions: {}
-        }}}, function(err, result) {
-            if(err) console.error(err);
-        });
-        
-        emojis.forEach(v => (msg ? msg.react(v).catch(err => {
-            let channel = msg.channel;
-            msg.delete();
-    
-            channel.send("Something went wrong. Please make sure you are using valid emojis");
-        }) : ""));
-    
-        pollCollect(msg, {});
+        interaction.reply("Use the text variation instead. Discord doesn't support variadic arguments in slash commands")
     },
     executeText: async function(msg, args) {
         let prefix = (await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id })).prefix;
