d8e658f075420550aa42d8c06e48315fba9f28a2
stranothus <stranothus@gmail.com>
2021-12-12 14:36:29 -0600

    Check current server mutes

---
+++ commands/mod/checkmutes.mjs
@@ -0,0 +1,40 @@
+import isAdmin from "../../utils/isAdmin.mjs";
+import { MessageEmbed } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("checkmutes")
+        .setDescription("See muted members"),
+    description: `checkmutes\` makes me list all current muted members`,
+    category: "moderation",
+    DMs: false,
+    execute: async function(interaction) {
+        if(!isAdmin(interaction)) return;
+
+        let guildDB = await DB.Guilds.collection("Info").findOne({ "id": interaction.guild.id });
+        let mutedRole = guildDB.moderation.muteRole;
+        let officialMutes = guildDB.members.filter(v => v.muted);
+        let unofficialMutes = (await msg.guild.roles.fetch(mutedRole)).members.filter(v => !(officialMutes.map(v => v.id).indexOf(v.user.id) + 1));
+
+        let reply = new MessageEmbed()
+            .setName("**Muted members**")
+            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${Math.floor((v.muted - Date.now()) / 1000)} seconds`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id} was muted by someone else`).join("\n"));
+    
+        interaction.reply({ embeds: [ reply ]});
+    },
+    executeText: async function(msg, args) {
+        if(!isAdmin(msg)) return;
+
+        let guildDB = await DB.Guilds.collection("Info").findOne({ "id": msg.guild.id });
+        let mutedRole = guildDB.moderation.muteRole;
+        let officialMutes = guildDB.members.filter(v => v.muted);
+        let unofficialMutes = (await msg.guild.roles.fetch(mutedRole)).members.filter(v => !(officialMutes.map(v => v.id).indexOf(v.user.id) + 1));
+        
+        let reply = new MessageEmbed()
+            .setTitle("**Muted members**")
+            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${Math.floor((v.muted - Date.now()) / 1000)} seconds`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id}> was muted by someone else`).join("\n"));
+    
+        msg.channel.send({ embeds: [ reply ]});
+    }
+};
