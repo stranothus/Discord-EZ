2fa4206f4b3ece2b378aba694f40bfd5468db4e1
stranothus <stranothus@gmail.com>
2021-12-12 15:12:57 -0600

    Relative time units Time units adjust when it makes sense. Times larger than 60 seconds becomes minutes. Time longer than 60 minutes becomes hours.

--- commands/mod/checkmutes.mjs
+++ commands/mod/checkmutes.mjs
@@ -18,8 +18,16 @@ export default {
         let unofficialMutes = (await msg.guild.roles.fetch(mutedRole)).members.filter(v => !(officialMutes.map(v => v.id).indexOf(v.user.id) + 1));
 
         let reply = new MessageEmbed()
-            .setName("**Muted members**")
-            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${Math.floor((v.muted - Date.now()) / 1000)} seconds for ${(v.infractions.reverse().filter(v => v.includes("Muted for"))[0] || "\"No reason\"").replace(/^Muted for/i, "")}`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id} was muted by someone else`).join("\n"));
+            .setTitle("**Muted members**")
+            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${(() => {
+                let time = v.muted - Date.now();
+
+                if(time >= 1000 * 60 * 60) time = Math.floor(time / (1000 * 60 * 60)) + " hours"
+                if(time >= 1000 * 60) time = Math.floor(time / (1000 * 60)) + " minutes"
+                if(time >= 1000) time = Math.floor(time / 1000) + " seconds"
+
+                return time
+            })()} for ${(v.infractions.reverse().filter(v => v.includes("Muted for"))[0] || "\"No reason\"").replace(/^Muted for/i, "")}`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id}> was muted by someone else`).join("\n"));
     
         interaction.reply({ embeds: [ reply ]});
     },
@@ -33,7 +41,15 @@ export default {
         
         let reply = new MessageEmbed()
             .setTitle("**Muted members**")
-            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${Math.floor((v.muted - Date.now()) / 1000)} seconds for ${(v.infractions.reverse().filter(v => v.includes("Muted for"))[0] || "\"No reason\"").replace(/^Muted for/i, "")}`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id}> was muted by someone else`).join("\n"));
+            .setDescription(officialMutes.map(v => `<@!${v.id}> is muted for ${(() => {
+                let time = v.muted - Date.now();
+
+                if(time >= 1000 * 60 * 60) time = Math.floor(time / (1000 * 60 * 60)) + " hours"
+                if(time >= 1000 * 60) time = Math.floor(time / (1000 * 60)) + " minutes"
+                if(time >= 1000) time = Math.floor(time / 1000) + " seconds"
+
+                return time
+            })()} for ${(v.infractions.reverse().filter(v => v.includes("Muted for"))[0] || "\"No reason\"").replace(/^Muted for/i, "")}`).join("\n") + "\n" + unofficialMutes.map(v => `<@!${v.user.id}> was muted by someone else`).join("\n"));
     
         msg.channel.send({ embeds: [ reply ]});
     }
